68ef3673a129c465c1b848c9bd64c1efef779286
Daniel-K-Ivanov <daniel.k.ivanov95@gmail.com>
2023-02-28 15:19:04 +0200

    feat(light-client-module): finish implementation
    Signed-off-by: Daniel-K-Ivanov <daniel.k.ivanov95@gmail.com>

---
+++ src/constants/constants.ts
@@ -0,0 +1,6 @@
+export const ROOT_BYTE_LENGTH = 32;
+export const AGGREGATE_SIGNATURE_BYTE_LENGTH = 96;
+export const SECONDS_PER_SLOT: number = 12;
+export const SYNC_COMMITTEE_SIZE: number = 512;
+export const SLOTS_PER_SYNC_PERIOD: number = 8192;
+export const MIN_SYNC_COMMITTEE_PARTICIPATION = Math.ceil(SYNC_COMMITTEE_SIZE * 2 / 3);
